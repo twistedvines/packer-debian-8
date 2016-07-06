5341158a8dd048b7e7af99fc455f1c9bf10ad0c5
Harry Owen <harry@twistedvin.es>
2016-07-06 20:31:16 +0100

    Add test for puppet installation

--- scripts/install_puppet.sh
+++ scripts/install_puppet.sh
@@ -7,3 +7,14 @@ echo "Installing Puppet Agent..."
   apt-get install -qq puppet-agent
   rm -r $PUPPET_RELEASE
 } > /dev/null 2>&1
+
+echo "Testing Puppet has been installed correctly..."
+{
+  PUPPET_OUTPUT=`/opt/puppetlabs/puppet/bin/puppet --version`
+  if [[ $? -eq 0 ]]; then
+    echo "All good! you're rolling with version ${PUPPET_OUTPUT}."
+  else
+    echo "Something must have gone wrong, here's the output:"
+    echo $PUPPET_OUTPUT
+  fi
+}
